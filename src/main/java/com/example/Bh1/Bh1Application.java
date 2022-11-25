e2497357778b1dc9638213e07a1b31469a3771e1
ByHorizon <rptedpa4@naver.com>
2022-11-25 20:28:09 +0900

    Initial commit

---
+++ src/main/java/com/example/Bh1/Bh1Application.java
@@ -0,0 +1,13 @@
+package com.example.Bh1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bh1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bh1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Bh1/Bh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bh1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bh1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
